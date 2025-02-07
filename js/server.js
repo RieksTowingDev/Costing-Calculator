3e01d6a155d7caaa7a9367396b36d7edd7ff12c4
RieksTowingDev <dev@riekstowing.com>
2025-02-07 11:45:08 +0200

    forgot to save lel

--- js/server.js
+++ js/server.js
@@ -61,13 +61,14 @@ app.post('/submit', async (req, res) => {
         request.input('totalVAT', sql.Float, totalVAT);
         request.input('Client', sql.VarChar, client);
         request.input('Jobnr', sql.Int, jobnr);
-        const [rows] = await db.query(`SELECT MAX(id) AS last_id FROM my_table`);
+        const [rows] = await db.query(`SELECT MAX(qoutenr) AS last_id FROM CalculatedCosts`);
         const nextId = (rows[0].last_id || 0) + 1;
+        const qoutenr = "Q00" + nextId;
         const date = Date.now();
         console.log(vehicle, callOut, calloutTotal, Traveltotal, labourTotal, towtotal, ADDtotal, Tolltotal, totalExclVAT, totalVAT, client, jobnr);
         await request.query(`
-            INSERT INTO CalculatedCosts (vehicle, callout, callouttotal, perkm, labourprep, kmtowed, additional, toll, totalexlvat, totalinclvat, client, jobrn)
-            VALUES ('${vehicle}', @CallOut, @calloutTotal, @Traveltotal, @labourTotal, @towtotal, @ADDtotal, @Tolltotal, @totalExclVAT, @totalVAT, @Client, @Jobnr)
+            INSERT INTO CalculatedCosts (date, fullqoutenr, qoutenr,vehicle, callout, callouttotal, perkm, labourprep, kmtowed, additional, toll, totalexlvat, totalinclvat, client, jobrn)
+            VALUES (@date, @qoutenr, @nextId, '${vehicle}', @CallOut, @calloutTotal, @Traveltotal, @labourTotal, @towtotal, @ADDtotal, @Tolltotal, @totalExclVAT, @totalVAT, @Client, @Jobnr)
         `);
 
         res.json({ message: 'Data saved successfully', totalVAT});
