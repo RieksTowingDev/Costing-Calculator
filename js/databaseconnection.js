f98f7e78e6dc39033499f2d65bf45a31f9873d76
RieksTowingDev <dev@riekstowing.com>
2025-02-11 09:10:04 +0200

    wrangler added

--- js/databaseconnection.js
+++ js/databaseconnection.js
@@ -14,7 +14,26 @@ const config = {
 
 async function connectToDatabase() {
     try {
-        await sql.connect(config);
+        export default {
+  async fetch(request, env) {
+    const { pathname } = new URL(request.url);
+
+    if (pathname === "/api/beverages") {
+      // If you did not use `DB` as your binding name, change it here
+      const { results } = await env.DB.prepare(
+        "SELECT * FROM Customers WHERE CompanyName = ?",
+      )
+        .bind("Bs Beverages")
+        .all();
+      return Response.json(results);
+    }
+
+    return new Response(
+      "Call /api/beverages to see everyone who works at Bs Beverages",
+    );
+  },
+};
+        //await sql.connect(config);
         console.log('Connected to the database successfully');
     } catch (err) {
         console.error('Database connection failed: ', err);
